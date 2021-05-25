75cc2b4abe180e7442d3caa41b5457f91c177e91
David <unbekannt.ghost@gmail.com>
2021-05-25 08:51:15 +0200

    add ws

---
+++ build/server.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//import express = require('express');
+var express_1 = __importDefault(require("express"));
+//import cors from 'cors';
+// Server erzeugen
+var server = express_1.default();
+// Static Content aktivieren
+// Beispiel: http://localhost:8080/index.html
+server.use(express_1.default.static('public'));
+// JSON Body parser aktivieren
+server.use(express_1.default.json());
+// CORS aktivieren
+//server.use (cors);
+server.get('/api/echo', function (request, response) {
+    response.send('Hello NodeJS');
+});
+var port = 8080;
+server.listen(port, function () {
+    console.log("API is listening on port " + port);
+});
